bcbbc15f0b12a9ffdd07421411772d6aa7586785
bminczu <bminczu@gmail.com>
2021-12-16 11:11:07 -0600

    first heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :users
   resources :reviews
   resources :posts
-
-
+  
+  
   namespace :api do 
     namespace :v1 do 
       resources :users
